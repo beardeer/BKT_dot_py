84714d576b7e1341e3a07f8d5a7754c0116fcd30
Xiaolu Xiong <beardeer@gmail.com>
2016-03-07 08:34:48 -0500

    first files

---
+++ BKT.py
@@ -0,0 +1,18 @@
+from sklearn.base import BaseEstimator
+
+class BKT(BaseEstimator):
+    def __init__(sekf):
+        pass
+
+    def fit(self, X, y):
+        return self
+
+    def predict(self, X):
+        return
+
+
+if __name__ == "__main__":
+    from sklearn.base import BaseEstimator
+    pass
+
+
